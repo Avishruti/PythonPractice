9eea8b448ce6f73afa81f59dd80942775424f144
Shruti Singh <111shrutising@gmail.com>
2024-06-19 22:38:27 +0530

    init

--- gfg/CheckTheStatus.py
+++ gfg/CheckTheStatus.py
@@ -1,2 +1,11 @@
+# Read input values
 a = int(input())
-b =
+b = int(input())
+flag = True
+# Check the conditions
+if (a >= 0 and b < 0 and not flag) or (a < 0 and b >= 0 and not flag):
+    print("True")
+elif a < 0 and b < 0 and flag:
+    print("True")
+else:
+    print("end")
