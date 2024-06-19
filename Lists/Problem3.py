8f9865a20232724f1b8b0bc17600976644444d62
Shruti Singh <111shrutising@gmail.com>
2024-06-19 22:02:23 +0530

    init

--- Lists/Problem3.py
+++
@@ -1,8 +0,0 @@
-#Write a program which can compute the factorial of a given numbers.
-#The results should be printed in a comma-separated sequence on a single line.
-
-num = []
-for i in range(2000,3201):
-    if(i%7==0 ) & (i%5 != 0):
-        num.append(i)
-print(list(num))
