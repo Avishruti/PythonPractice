3071d1d0f40784dbca624960d7046bde94cffb02
Shruti Singh <111shrutising@gmail.com>
2024-06-20 00:10:18 +0530

    init

---
+++ gfg/ZeroConverter.py
@@ -0,0 +1,15 @@
+n = int(input("Enter a number: "))
+if n < 0:
+    while n <= 0:
+        print(n, end=' ')
+        n = n + 1
+else:
+    while n > 0:
+        n = n - 1
+        print(n, end=' ')
+
+
+
+
+
+
